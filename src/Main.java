121dc7e0e82f481c57ac2f031f8f11095b0e3b4c
Mohd Ghouse uddin <mohdshaibaz7266@gmail.com>
2023-03-04 19:16:53 +0530

    Initial Commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        int sum = a + b;
+        int sub = a - b;
+        int mul = a * b;
+        int div = a / b;
+        int mod = a % b;
+        System.out.println("The addition is "+sum);
+        System.out.println("The substraction is "+sub);
+        System.out.println("The multiplication is "+mul);
+        System.out.println("The division is "+div);
+        System.out.println("The modulus is "+mod);
+
+    }
+}
