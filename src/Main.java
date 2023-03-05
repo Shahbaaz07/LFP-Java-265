431692c74d17029f47ebab88953fbfcf7d294581
Mohd Ghouse uddin <mohdshaibaz7266@gmail.com>
2023-03-06 01:34:10 +0530

    Added employee wage

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        int a = 1;
-        int b = 2;
-        int sum = a + b;
-        int sub = a - b;
-        int mul = a * b;
-        int div = a / b;
-        int mod = a % b;
-        System.out.println("The addition is "+sum);
-        System.out.println("The substraction is "+sub);
-        System.out.println("The multiplication is "+mul);
-        System.out.println("The division is "+div);
-        System.out.println("The modulus is "+mod);
+        int wagePerHour = 20;
+        int fullDayHours = 8;
+        int dailyWage = wagePerHour * fullDayHours;
+        System.out.println(" daily employee wage is: " + dailyWage);
 
     }
 }
