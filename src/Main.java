f74d200888667b9012b07cdfb3567f318004e079
Mohd Ghouse uddin <mohdshaibaz7266@gmail.com>
2023-03-06 19:18:34 +0530

    Added employee part-time

--- src/Main.java
+++ src/Main.java
@@ -3,12 +3,22 @@ import java.util.Random;
 public class Main{
     public static void main(String[] args) {
         Random rand = new Random();
-        int attendance = rand.nextInt(2);
+        int attendance = rand.nextInt(3);
 
         if (attendance == 0) {
             System.out.println("Employee is absent");
-        } else {
-            System.out.println("Employee is present");
+        } else if (attendance == 1){
+            int wage = 500;
+            int workingHours = 8;
+            int totalWage = wage * workingHours;
+            System.out.println("Employee is full time");
+            System.out.println("Wage per day is: Rs. " +totalWage);
+        } else{
+            int wage = 200;
+            int workingHours = 4;
+            int totalWage = wage * workingHours;
+            System.out.println("Employee is part-time");
+            System.out.println("Wage per day is: Rs. " +totalWage);
         }
     }
 }
