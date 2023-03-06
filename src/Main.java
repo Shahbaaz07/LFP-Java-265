5870f8cb5824fb629696d2427dbe462de7da727d
Mohd Ghouse uddin <mohdshaibaz7266@gmail.com>
2023-03-06 18:33:53 +0530

    Added employee attendance

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
-public class Main {
-    public static void main(String[] args) {
-        int wagePerHour = 20;
-        int fullDayHours = 8;
-        int dailyWage = wagePerHour * fullDayHours;
-        System.out.println(" daily employee wage is: " + dailyWage);
+import java.util.Random;
 
+public class Main{
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int attendance = rand.nextInt(2);
+
+        if (attendance == 0) {
+            System.out.println("Employee is absent");
+        } else {
+            System.out.println("Employee is present");
+        }
     }
 }
