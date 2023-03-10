6aa7ec961784ecf51d56f10d1bc0d12f3c91eb95
Mohd Ghouse uddin <mohdshaibaz7266@gmail.com>
2023-03-10 16:31:31 +0530

    Added ArrayElements

---
+++ src/ArrayElements.java
@@ -0,0 +1,12 @@
+public class ArrayElements {
+
+        public static void main(String[] args) {
+
+            int[] numbers = { 1, 2, 3, 4, 5 };
+
+            for (int i = 0; i < numbers.length; i++) {
+                System.out.println(numbers[i]);
+            }
+        }
+
+}
